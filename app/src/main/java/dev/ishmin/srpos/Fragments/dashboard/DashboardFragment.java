cc116610782e5d2456a3c3a15e33e9e70ffbc7b5
Ishmin <54061319+ishminsingh@users.noreply.github.com>
2020-05-05 17:50:46 +0530

    ...

--- app/src/main/java/dev/ishmin/srpos/Fragments/dashboard/DashboardFragment.java
+++ app/src/main/java/dev/ishmin/srpos/Fragments/dashboard/DashboardFragment.java
@@ -11,23 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import com.github.mikephil.charting.charts.LineChart;
+
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LegendEntry;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+
 
 import java.util.ArrayList;
 
 import dev.ishmin.srpos.R;
 import dev.ishmin.srpos.TryOutFile;
+
 
 public class DashboardFragment extends Fragment {
 
@@ -40,10 +37,6 @@ public class DashboardFragment extends Fragment {
     private String[] xData = {"Paid", "UnPaid"};
     int[] legendColors = new int[] {Color.MAGENTA, Color.YELLOW};
     PieChart pieChart;
-    LineChart chart;
-    private int[] yValues = {50, 100, 150, 200, 250, 300, 350, 400, 450, 500};
-    private String[] xValues = {"2020-04-01", "2020-04-02", "2020-04-03", "2020-04-04", "2020-04-05",
-            "2020-04-06", "2020-04-07", "2020-04-08", "2020-04-09", "2020-04-10"};
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
@@ -61,9 +54,14 @@ public class DashboardFragment extends Fragment {
         addDataSet();
 
         cardSales = v.findViewById(R.id.cardViewSales);
-        chart = v.findViewById(R.id.lineChart);
-        chart.getDescription().setText("Sales");
-        addDataSet2();
+        cardSales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), TryOutFile.class);
+                startActivity(i);
+            }
+        });
+
         return v;
     }
 
@@ -113,38 +111,5 @@ public class DashboardFragment extends Fragment {
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
         pieChart.invalidate();
-    }
-    private void addDataSet2(){
-
-        final ArrayList<Entry> yData = new ArrayList<>();
-        final ArrayList<String> xData = new ArrayList<>();
-
-        for(int i = 0; i < yValues.length; i++){
-            yData.add(new Entry(yValues[i], i));
-        }
-        for(int i = 0; i < xValues.length; i++){
-            xData.add(xValues[i]);
-        }
-        //create line dataSet
-        LineDataSet lineDataSet = new LineDataSet(yData, "Sales");
-        lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setGranularity(1f);
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setEnabled(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setPosition(XAxis.XAxisPosition.TOP);
-
-        //        chart.getXAxis().setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
-
-
-        //create line data object
-        LineData lineData = new LineData(lineDataSet);
-        lineData.setValueTextSize(12f);
-        lineData.setValueTextColor(Color.BLACK);
-
-        chart.setData(lineData);
-        chart.invalidate();
     }
 }
